1a8f44c975546f56cbf7a26817e747a057289a44
eney <hkc9133@naver.com>
2020-12-01 00:09:35 +0900

    수정

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import Link from "next/link";
-import Head from 'next/Head';
+import {Head } from 'next/document'
 
 const Index = () => {
     return (
         <>
-            <Head >
-                <title>My page title</title>
-                <script src="https://polyfill.io/v3/polyfill.min.js?features=es6,es7,es8,es9,NodeList.prototype.forEach&flags=gated" />
-            </Head>
             <div>
                 Welcome to Next.js!
                 <Link href="/admin/dashboard"><a>대시보드 이동</a></Link>
